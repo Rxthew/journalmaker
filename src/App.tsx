32d763059b7315c6a57644ca018e8a784413d11c
rxthew <richardtheumaster@gmail.com>
2022-06-23 19:14:58 +0200

    Test functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
+type appProps = {
+  something : string
+}; 
+
+type appState = {
+  somethingElse:string;
+}
+
+class App extends React.Component<appProps,appState> { 
+  state : appState = {
+    somethingElse : 'friend'
       
+  }
+
+  render(){
+    return (
+    <div>
+        <h1>Hello {this.state.somethingElse}</h1>
+        <p>{this.props.something}</p>
     </div>
-  );
+    )   
+  }
+    
 }
 
 export default App;
