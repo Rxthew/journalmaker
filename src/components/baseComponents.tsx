d2a875bba73f9bd6a1cbb6b30e8e992ec4e2fbdb
rxthew <richardtheumaster@gmail.com>
2022-06-24 00:46:21 +0200

    Initialise baseComponents.tsx

---
+++ src/components/baseComponents.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type baseProps = {
+    click() : void
+}
+
+type baseState = {
+    content : string
+}
+
+class readElement extends React.Component{
+
+}
+
+class FormElement extends React.Component{
+
+}
+
+
+class Pliable extends React.Component {
+
+}
+
+export default Pliable
