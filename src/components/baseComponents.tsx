4f9c65d5919252e01c48df50e8a87381653344ff
rxthew <richardtheumaster@gmail.com>
2022-06-24 22:41:40 +0200

    Initialise FormElement basic structure

--- src/components/baseComponents.tsx
+++ src/components/baseComponents.tsx
@@ -1,19 +1,62 @@
 import React from 'react'
 
 type baseProps = Readonly<{
-    click() : void
+    action() : void
 }>
 
 type baseState = Readonly<{
     content : string
 }>
 
+type formProps = baseProps & Readonly<{
+      push(): void,
+      cancel(): void,
+      labelfor: string,
+      requiredprop: string,
+      itemid : string,
+}>
+
 class readElement extends React.Component{
 
 }
 
-class FormElement extends React.Component<baseProps, baseState>{
-    
+class FormElement extends React.Component<formProps, baseState>{
+    constructor(props:formProps){
+        super(props)
+
+        this.handleContent.bind(this)
+        this.handlePush.bind(this)
+        this.handleCancel.bind(this)
+
+        this.state = {
+            content: ''
+        }
+    }
+
+    handleContent(){
+
+    }
+
+    handlePush(){
+
+    }
+
+    handleCancel(){
+
+    }        
+
+    render(){
+        return (
+            <div>
+                <button></button>
+                <form>
+                    <label></label>
+                    <input />
+                    <button></button>
+                </form>
+            </div>
+        )
+    }
 
 
 }
