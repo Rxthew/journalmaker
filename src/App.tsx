ca4314a7fb36f15471b37188f9af17e2e0abf50f
rxthew <richardtheumaster@gmail.com>
2022-06-26 18:09:59 +0200

    Put elementList housing elems in App state
    (Remove detritus too)

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import './App.css';
+import {ReadElement,ParagraphElement,TitleElement} from './components/baseComponents'
 
-type appProps = {
-  something : string
-}; 
 
-type appState = {
-  somethingElse:string;
-}
+type appState = Readonly<{
+  elementList : React.ReactElement[];
+}>
 
-class App extends React.Component<appProps,appState> {
-  constructor(props:appProps){
+class App extends React.Component<{},appState> {
+  appRoot: React.ReactElement
+  constructor(props:{}){
     super(props);
     this.state = {
-      somethingElse : 'friend'      
+      elementList : [<ReadElement currentValue='John Doe' element={TitleElement} textCols={20} textRows={20}/>]           
     }
+    this.appRoot = <div>{this.state.elementList}</div>
   } 
-  
-
   render(){
     return (
-    <div>
-        <h1>Hello {this.state.somethingElse}</h1>
-        <p>{this.props.something}</p>
-    </div>
+        this.appRoot
     )   
   }
     
