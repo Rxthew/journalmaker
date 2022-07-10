b1f7914bf3aeb0c566b769fb9de56b551fb702c0
rxthew <richardtheumaster@gmail.com>
2022-07-11 01:46:37 +0200

    Introduce dummy text

--- src/components/templateEntry.tsx
+++ src/components/templateEntry.tsx
@@ -16,19 +16,22 @@ class JournalEntry extends React.Component<entryProps>{
             <div>
                 <dl>
                     <dt>
-                    <ReadElement element={TitleElement} currentValue=''/>
+                    <ReadElement element={TitleElement} currentValue='Title of Entry'/>
                     </dt>
                     <dt>
                     <ReadElement element={ParagraphElement} currentValue='Date:'/>
                     </dt>
                     <dd>
-                    <ReadElement element={ParagraphElement} currentValue=''/>
+                    <ReadElement element={ParagraphElement} currentValue='Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                     Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. '/>
                     </dd>
                 </dl>
                 <button onClick={this.props.del} id={this.props.id}><Delete /></button>
             </div>
         )
     }
-} 
+}
+
+
 
 export default JournalEntry
